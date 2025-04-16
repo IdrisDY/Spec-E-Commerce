46567294b1f2d74bd588f5eca304277476ed508a
DisuAde <idrisadekunle726@gmail.com>
2025-04-16 17:59:59 +0100

    add poppins

--- app.vue
+++ app.vue
@@ -6,3 +6,14 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script lang="ts">
+useHead({
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap",
+    },
+  ],
+});
+</script>
